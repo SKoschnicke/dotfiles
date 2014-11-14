d6c8fc46c4b770ab07646dd7161cfd2ba12cd533
Sven Koschnicke <s.koschnicke@gfxpro.com>
2014-11-14 09:31:04 +0100

    tmux with plugins

--- setup.sh
+++ setup.sh
@@ -4,6 +4,11 @@ ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/gvimrc ~/.gvimrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+# you need tmux >=1.9:
+# sudo add-apt-repository -y ppa:pi-rho/dev
+# sudo apt-get update
+# sudo apt-get install -y tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s ~/.dotfiles/screenrc ~/.screenrc
 ln -s ~/.dotfiles/Xresources ~/.Xresources
 ln -s ~/.dotfiles/emacs ~/.emacs
