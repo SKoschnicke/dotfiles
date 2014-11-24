1a979dd9b6adfc51825f5dda6856e1a85af030f1
Sven Koschnicke <s.koschnicke@gfxpro.com>
2014-11-24 21:47:56 +0100

    no emacs config anymore

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ ln -s ~/.dotfiles/gvimrc ~/.gvimrc
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/screenrc ~/.screenrc
 ln -s ~/.dotfiles/Xresources ~/.Xresources
-ln -s ~/.dotfiles/emacs ~/.emacs
-ln -s ~/.dotfiles/emacs.d ~/.emacs.d
+#ln -s ~/.dotfiles/emacs ~/.emacs
+#ln -s ~/.dotfiles/emacs.d ~/.emacs.d
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 mkdir -p ~/.xmonad
 ln -s ~/.dotfiles/xmonad.hs ~/.xmonad/xmonad.hs
