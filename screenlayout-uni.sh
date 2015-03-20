81bb0615adbe8b5c0ad3b5c5e6e534f902c563d4
Sven Koschnicke <s.koschnicke@gfxpro.com>
2015-03-20 13:18:17 +0100

    added screenlayout generated by arandr

---
+++ screenlayout-uni.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-3 --off --output Unknown-0 --off --output DVI-I-0 --off --output HDMI-0 --off --output DP-5 --off --output DP-4 --mode 3840x2160 --pos 2160x596 --rotate normal --output DVI-I-1 --off --output DP-2 --mode 3840x2160 --pos 0x0 --rotate left --output DP-1 --off --output DP-0 --off
