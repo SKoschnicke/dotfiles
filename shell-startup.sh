bba7c525a63289ab561ffbc02e4b8bb4889c496b
Sven Koschnicke <s.koschnicke@gfxpro.com>
2018-06-11 05:46:08 +0200

    replaced yaourt with trizen

--- shell-startup.sh
+++ shell-startup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 tmux new-session -d -s startup -n first
 # start the day with updates
-tmux send-keys -t startup:first "yaourt -Syu" Enter
+tmux send-keys -t startup:first "trizen -Syu" Enter
 tmux attach -t startup:first
