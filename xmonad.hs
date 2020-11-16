845a4de1a8f724f25dd1f3693df4a94c3373c2c3
Sven Koschnicke <s.koschnicke@gfxpro.com>
2020-11-16 07:31:25 +0100

    Remap screen lock keyboard shortcut

--- xmonad.hs
+++ xmonad.hs
@@ -137,7 +137,7 @@ myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
      spawn $ XMonad.terminal conf)
 
   -- Lock the screen using xscreensaver.
-  , ((modMask .|. shiftMask, xK_l),
+  , ((modMask, xK_b),
      spawn "xscreensaver-command -lock")
 
   -- switch between keyboard layouts
